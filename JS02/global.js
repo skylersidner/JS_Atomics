89945d2083d06d289b0c31932d8c2630c957d3f6
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-04 20:56:47 -0600

    Completed 2

---
+++ JS02/global.js
@@ -0,0 +1,17 @@
+alert("Hello!  I'm Sky and here's some stuff about me.");
+console.log("My favorite breakfast is some perfectly poached eggs with toast, fresh fruit, and a little bacon.  Delicious!");
+console.log("Currently, I don't live with any cats--only two dogs.  I can tell you that from the barking alone... I really prefer cats.");
+console.log("My favorite place to eat right now is Chipotle!  So much amazing stuff there.");
+console.log("I used to take naps almost every day, but it was ruining my sleep cycle.");
+console.log("I'm delighted to say I'm currently nap-free, and I couldn't be happier!")
+alert("And that's it!  Have a good day!");
+
+// Just to see which works and which doesn't, and whether my assumptions are correct. //
+
+alert( "1 + 1");
+alert( 1 + 1 );
+alert( "1" + "1" );
+alert( "1" + 1 );
+alert( 1 + "1" );
+
+// ...and I was correct for each one.  Not such a mystery at this point! //
